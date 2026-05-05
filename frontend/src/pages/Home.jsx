8fec7fee1593fcdc1a431b4b711c5167b4062353
benx <benx@benxs-iMac.local>
2026-05-05 16:46:02 +0300

    update hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import QuikFilter from '../components/QuikFilter/QuikFilter'
-import ProductOverView from '../components/productOverView/productOverView'
+
 
 const Home = () => {
   return (
     <div className=''>
         <Hero/>
         <QuikFilter/>
-        <ProductOverView/>
+       
     </div>
   )
 }
