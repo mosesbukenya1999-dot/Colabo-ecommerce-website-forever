57a8974b967983f0190a1dd123706a034f728fce
benx <benx@benxs-iMac.local>
2026-05-05 14:08:33 +0300

    fix build error remove product overview

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import QuikFilter from '../components/QuikFilter/QuikFilter'
-/* import ProductOverView from '../components/productOverView/productOverView' */
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <QuikFilter/>
-        {/* <ProductOverView/> */}
     </div>
   )
 }
