ad9831fed8e25ab669ecff9e9e943cfa95a1fe08
benx <benx@benxs-iMac.local>
2026-05-05 14:02:35 +0300

    temporary fix: remove product overview for deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import QuikFilter from '../components/QuikFilter/QuikFilter'
-import ProductOverView from '../components/productOverView/productOverView'
+/* import ProductOverView from '../components/productOverView/productOverView' */
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <QuikFilter/>
-        <ProductOverView/>
+        {/* <ProductOverView/> */}
     </div>
   )
 }
