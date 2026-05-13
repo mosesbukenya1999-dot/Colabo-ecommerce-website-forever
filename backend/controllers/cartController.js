56674cdbbdb60e0d8a2e23dcfb840cf4227cb062
benx <benx@benxs-iMac.local>
2026-05-13 16:19:05 +0300

    updated backend

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -40,9 +40,18 @@ const addToCart = async (req,res)=> {
 const getCart = async (req,res)=> {
     try {
         
-        const {itemId,sizes,userId} = req.body;
 
+        const userId = req.user.id;
+
+        if (!userId) {
+            return res.json({success:false, message: "User does not exist"})
+        };
         
+        const userData = await userModel.findById(userId);
+
+        const cartData = userData.cartData || {};
+
+        res.json({success:true, cartData});
 
     } catch (error) {
         console.log(error);
@@ -53,9 +62,23 @@ const getCart = async (req,res)=> {
 const updateCart = async (req,res)=> {
     try {
         
-        const {itemId,sizes,userId} = req.body;
+        const {itemId,sizes,quantity} = req.body;
 
+        const userId = req.user.id;
+
+        if (!userId) {
+            return res.json({success:false, message: "User does not exist"})
+        };
         
+        const userData = await userModel.findById(userId);
+
+        const cartData = userData.cartData || {};
+
+        cartData[itemId][sizes]= quantity;
+
+        await userModel.findByIdAndUpdate(userId, {cartData});
+
+        res.json({success:true, message: "Cart Updated"})
 
     } catch (error) {
         console.log(error);
