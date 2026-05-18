25b9b1f5def4f4290762df40f3a7dc82b5734808
benx <benx@benxs-iMac.local>
2026-05-18 14:06:38 +0300

    updated backendUrl

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,15 +70,8 @@ const loginUser = async (req,res)=>{
 
 const getCurrentUser = async (req, res) => {
     try {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
-        return res.json({ success: false, message: "No token provided" });
-      }
   
-      const token = authHeader.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  
-      const user = await userModel.findById(decoded.id).select("-password"); // exclude password
+      const user = await userModel.findOne({email}) // exclude password
   
       if (!user) return res.json({ success: false, message: "User not found" });
   
