be962139e55cad36ccf3d3dbd6808b741503d205
benx <benx@benxs-iMac.local>
2026-05-19 11:02:25 +0300

    pics in reviews

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -2,56 +2,37 @@ import reviewModel from "../models/reviewModel.js";
 import userModel from "../models/userModel.js";
 
 
-const addReview = async(req,res)=> {
+const addReview = async (req, res) => {
     try {
-        
-        const {rating,text, productId} = req.body;
-
+        const { rating, text, productId } = req.body;
         const userId = req.user.id;
 
-        if (!userId) {
-            return res.json({success:false, message: "User not Found"})
-        };
+        if (!userId) return res.json({ success: false, message: "User not found" });
 
-        const user = await userModel.findById(userId);
+        const newReview = new reviewModel({ rating, text, productId, userId });
+        await newReview.save();
 
-        if (!user) {
-            return res.json({success:false, message: "User does not exist"})
-        }
-
-        const reviewData = {
-            rating,
-            text,
-            userName: user.name,
-            productId,
-            userId
-        };
-
-        const newReviews = new reviewModel(reviewData);
-
-        await newReviews.save();
-
-        res.json({success: true, message: "Review Submitted"})
-
+        res.json({ success: true, message: "Review submitted" });
     } catch (error) {
         console.log(error);
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message });
     }
-}
+};
 
-const getReviews = async(req,res)=> {
+const getReviews = async (req, res) => {
     try {
+        const { productId } = req.params;
 
-        const {productId} = req.params;
-        
-        const reviews = await reviewModel.find({productId});
+        const reviews = await reviewModel
+            .find({ productId })
+            .populate("userId", "name profilePic") // populate only name & profilePic
+            .sort({ createdAt: -1 });
 
-        res.json({success:true, reviews})
-
+        res.json({ success: true, reviews });
     } catch (error) {
         console.log(error);
-        res.json({success:false, message: error.message})
+        res.json({ success: false, message: error.message });
     }
-}
+};
 
 export { addReview, getReviews}
