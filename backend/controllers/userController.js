4cccee070d37b330f8e0ac22262a2f9c833709bf
benx <benx@benxs-iMac.local>
2026-05-20 20:02:25 +0300

    updated backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -87,6 +87,7 @@ const adminLogin = async (req,res)=>{
 
         if (email=== process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
             const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({success:true, token})
         }else{
             res.json({success:false, message: "Invalid Match!!!"})
         }
--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,11 +2,12 @@
 import express from "express";
 import { addProduct, listProduct, removeProduct } from "../controllers/ProductController.js";
 import upload from "../middlewares/multer.js";
+import adminAuth from "../middlewares/adminAuth.js";
 
 const router = express.Router();
 
 router.post(
-  "/add",
+  "/add",adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -16,7 +17,7 @@ router.post(
   addProduct
 );
 
-router.get("/list", listProduct);
-router.post("/remove", removeProduct);
+router.get("/list", adminAuth,listProduct);
+router.post("/remove", adminAuth,removeProduct);
 
 export default router;
